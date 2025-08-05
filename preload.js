27704e973685e301997410248ce3fe1b8287e363
bazzapotato <71957020+bazzapotato@users.noreply.github.com>
2025-08-05 13:00:02 +1000

    Update preload.js
    added google drive funtions

--- preload.js
+++ preload.js
@@ -5,6 +5,15 @@ contextBridge.exposeInMainWorld('electronAPI', {
   getDataDirectory: () => ipcRenderer.invoke('get-data-directory'),
   getCustomers: () => ipcRenderer.invoke('get-customers'),
   saveCustomer: (customer) => ipcRenderer.invoke('save-customer', customer),
+   // Google Drive API methods
+  authorizeGoogleDrive: () => ipcRenderer.invoke('google-drive-authorize'),
+  setDriveFolder: (folderId) => ipcRenderer.invoke('set-drive-folder', folderId),
+  getDriveCustomers: () => ipcRenderer.invoke('get-drive-customers'),
+  saveDriveCustomer: (customer) => ipcRenderer.invoke('save-drive-customer', customer),
+  saveDriveFile: (fileData) => ipcRenderer.invoke('save-drive-file', fileData),
+  getBackupInterval: () => ipcRenderer.invoke('get-backup-interval'),
+  setBackupInterval: (minutes) => ipcRenderer.invoke('set-backup-interval', minutes),
+  triggerBackup: () => ipcRenderer.invoke('trigger-backup')
   saveInteraction: (interaction) => ipcRenderer.invoke('save-interaction', interaction),
   saveFile: (fileData) => ipcRenderer.invoke('save-file', fileData)
 })
